e08b1e47094fa2c9e09ff549f67a31e0b5cecddb
Oliver Giles <ogiles@email.com>
2022-02-22 16:32:29 +0000

    setup.py file

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 from setuptools import find_packages
 from setuptools import setup
+from pathlib import Path
 
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='vinegar',
       version="1.0",
-      description="Project Description",
+      description="CLI to convert data into pickled pandas DataFrames.",
       packages=find_packages(),
       install_requires=requirements,
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/vinegar-run'],
-      zip_safe=False)
+      zip_safe=False,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      entry_points='''
+      [console_scripts]
+      vinegar=vinegar.vinegar:vinegar
+      ''')
