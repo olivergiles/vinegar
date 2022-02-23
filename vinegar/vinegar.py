f92d1073072f3140b62b60c8fa2b62ebeb4551cd
Oliver Giles <ogiles@email.com>
2022-02-23 14:50:18 +0000

    added flag for whether the file includes an index

--- vinegar/vinegar.py
+++ vinegar/vinegar.py
@@ -5,14 +5,17 @@ import sys, os
 
 @click.command()
 @click.argument('file')
-def vinegar(file):
+@click.option('-i','--indexc', is_flag=True)
+def vinegar(file, indexc=None):
+    index = None
+    if indexc:
+        index = 0
     filetypes = ('.csv')
     if not file.endswith(filetypes):
         return 'Invalid filetype'
     if file.endswith('.csv'):
-        df = pd.read_csv(file,index_col=0)
+        df = pd.read_csv(file,index_col=index)
         pd.to_pickle(df, file.replace('.csv', '.pkl'))
-    #print(to_convert)
 
 if __name__ == '__main__':
     vinegar()
